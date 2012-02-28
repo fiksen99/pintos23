d02e14fbd41337f38601b522fd05a1e83ecc392d
Adam Fiksen <af1410@ic.ac.uk>
2012-02-28 19:57:00 +0000

    compiles, wait seems to work, not sure though.

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -488,6 +488,7 @@ init_thread (struct thread *t, const char *name, int priority)
   strlcpy (t->name, name, sizeof t->name);
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
+  list_init (&t->children);
 
   t->magic = THREAD_MAGIC;
 

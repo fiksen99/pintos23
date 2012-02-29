2ba4fd348087e45b330c3a476128f6c2fc281024
Sahil Jain <sdj09@ic.ac.uk>
2012-02-29 20:21:54 +0000

    working on system calls. see comments on execute_open

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -17,20 +17,36 @@
 #include "process.h"
 #include "threads/synch.h"
 #include "filesys/filesys.h"
+#include "devices/shutdown.h"
+#include "filesys/file.h"
 
 static void syscall_handler (struct intr_frame *);
+static void execute_halt (void);
 static void execute_exit (struct intr_frame *, int *);
 static void execute_exec (struct intr_frame *, const char **);
 static void execute_wait (struct intr_frame *, tid_t);
 static int execute_write (int, const void *, unsigned);
 static void execute_create (struct intr_frame *, const char *, unsigned);
 static void execute_remove (struct intr_frame *, const char *);
+static void execute_open (struct intr_frame *, const char *);
+static void execute_filesize (struct intr_frame *, int) UNUSED;
+static void execute_read (struct intr_frame *, int, void *, unsigned) UNUSED;
+static void execute_seek (int, unsigned) UNUSED;
+static void execute_tell (struct intr_frame *, int) UNUSED;
+static void execute_close (int) UNUSED;
 
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
+
+/* Implementation for file descriptor mapping */
+struct fd_elems
+{
+  int fd;
+  struct file *file;
+};
 
 static void
 syscall_handler (struct intr_frame *f) 
@@ -39,7 +55,7 @@ syscall_handler (struct intr_frame *f)
   int syscall = *stackptr;
   if( syscall == SYS_HALT )  /* ZERO arguments */
   { 
-//    shutdown_power_off();
+    execute_halt();
   } else if( syscall == SYS_EXIT || syscall == SYS_EXEC || syscall == SYS_WAIT ||
               syscall == SYS_REMOVE || syscall == SYS_OPEN || syscall == SYS_FILESIZE ||
               syscall == SYS_TELL || syscall == SYS_CLOSE )  /* ONE argument */
@@ -52,11 +68,11 @@ syscall_handler (struct intr_frame *f)
       execute_exec( f, (const char **)arg );
     } else if( syscall == SYS_WAIT )
     {
-//      pid_t pid = (pid_t)*arg;
+//    pid_t pid = (pid_t)*arg;
       execute_wait( f, (tid_t)*arg );
     } else if( syscall == SYS_REMOVE )
     {
-
+      execute_remove(f, (const char *)arg);
     } else if( syscall == SYS_OPEN )
     {
 
@@ -91,6 +107,16 @@ syscall_handler (struct intr_frame *f)
   thread_exit ();*/
 }
 
+/* Executes the halt system call. Terminates pintos by calling shutdown_power_off () */
+static void
+execute_halt (void)
+{
+  shutdown_power_off ();
+}
+
+/* Terminates the current user program, and sets eax of the intr_frame as the
+status of the kernel. A status of 0 indicates success, and nonzero values indicate
+errors */
 static void
 execute_exit (struct intr_frame * f, int *arg)
 {
@@ -144,7 +170,7 @@ execute_create (struct intr_frame *f, const char *file, unsigned initial_size)
 {
   bool *success = malloc(sizeof (bool));
   *success = filesys_create (file, initial_size);
-  f->eax = (uint32_t)success;
+  f->eax = (uint32_t) success;
 }
 
 static void
@@ -152,5 +178,59 @@ execute_remove (struct intr_frame *f, const char *file)
 {
   bool *success = malloc(sizeof (bool));
   *success = filesys_remove (file);
-  f->eax = (uint32_t)success;
+  f->eax = (uint32_t) success;
 }
+
+static void
+execute_open (struct intr_frame *f, const char *file)
+{
+  struct file *file_opened;
+  file_opened = filesys_open(file);
+  int *open = malloc(sizeof (int));
+  if(file_opened == NULL || file == NULL) // Returns -1 if file could not be opened
+  {
+    *open = -1;
+    f->eax = (uint32_t) open;
+  }
+
+  //WHAT DO I DO HERE?
+}
+
+static void
+execute_filesize (struct intr_frame *f UNUSED, int fd UNUSED)
+{
+  struct file *file;
+  
+  int *filesize;
+  if(file == NULL)
+  {
+    *filesize = 0;
+    f->eax = (uint32_t) filesize;
+  }
+  //do file_length
+  //eax = file_length
+}
+
+static void
+execute_read (struct intr_frame *f UNUSED, int fd UNUSED, void *buffer UNUSED, unsigned size UNUSED)
+{
+
+}
+
+static void
+execute_seek (int fd UNUSED, unsigned position UNUSED)
+{
+
+}
+
+static void
+execute_tell (struct intr_frame *f UNUSED, int fd UNUSED)
+{
+
+}
+
+static void
+execute_close (int fd UNUSED)
+{
+
+} 

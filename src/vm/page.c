24911bce5a09f4d13435c3b77a3fb3e79e583084
Adam Fiksen <af1410@ic.ac.uk>
2012-03-18 22:49:56 +0000

    created page.c and .h

---
+++ src/vm/page.c
@@ -0,0 +1,20 @@
+#include "vm/page.h"
+#include <hash.h>
+
+
+unsigned
+page_hash (const struct hash_elem *p_, void *aux UNUSED)
+{
+  const struct page *p = hash_entry (p_ struct page, hash_elem);
+  return hash_bytes (&p->addr, sizeof (p->addr));
+}
+
+bool
+page_less (const struct hash_elem *a_, const struct hash_elem *b_, void * aux UNUSED)
+{
+  const struct page *a = hash_entry (a_, struct page, hash_elem);
+  const struct page *b = hash_entry (b_, struct page, hash_elem);
+  return a->addr < b->addr;
+}
+
+
---
+++ src/vm/page.h
@@ -0,0 +1,27 @@
+#ifndef VM_PAGE_H
+#define VM_PAGE_H
+
+#include <hash.h>
+
+/* TODO
+*  add struct page to struct thread
+*  all the process.c stuff
+*/
+
+struct page
+{
+  struct hash_elem hash_elem; /* Has table element */
+  void *addr;                 /* Virtual address */
+  struct frame *frame;        /* Frame the page is stored in */
+
+};
+
+/* Returns a hash value for page p */
+unsigned
+page_hash (const struct hash_elem *, void *);
+
+/* Returns true if page a precedes page b. */
+bool
+page_less (const struct hash_elem *, const struct hash_elem *, void *);
+
+#endif

4b9f3b20689625c7a8a1f9fd412f9c5fdde13216
Adam Fiksen <af1410@ic.ac.uk>
2012-03-19 15:11:08 +0000

    started implementing frame table

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -17,8 +17,6 @@
 #include "threads/palloc.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
-#include <hash.h>
-#include "threads/malloc.h"
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);

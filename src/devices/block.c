29f2862af2525273bc61369f8c59deed4db1e187
Adam Fiksen <af1410@ic.ac.uk>
2012-03-22 14:21:59 +0000

    deleted printfs in block.c and other files, also modified setup_stack

--- src/devices/block.c
+++ src/devices/block.c
@@ -123,10 +123,7 @@ void
 block_read (struct block *block, block_sector_t sector, void *buffer)
 {
   check_sector (block, sector);
-/*  printf("for addr \"%p\" in page dir \"%p\" at the start of block_read\nlookup page returns: %p\n", (void *)0x8048000, (uint32_t *)0xc010d000, lookup_page ((uint32_t *)0xc010d000, (void *)0x8048000, false));
-  printf("LINE 128 IN BLOCK.C OVERWRITES PAGEDIR VALUE FFFUUUUUUUUUUUUUUUUU...\n\n");*/
-  block->ops->read (block->aux, sector, buffer);/*
-  printf("for addr \"%p\" in page dir \"%p\" at the end of block_read\nlookup page returns: %p\n\n", (void *)0x8048000, (uint32_t *)0xc010d000, lookup_page ((uint32_t *)0xc010d000, (void *)0x8048000, false));*/
+  block->ops->read (block->aux, sector, buffer);
   block->read_cnt++;
 }
 
